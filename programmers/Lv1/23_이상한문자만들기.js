2fd88b9b85d9e5b6a8cf6569507d543ac25f5f88
Hyebin-Woo <dev.hyebin@gmail.com>
2022-09-27 19:39:08 +0900

    9.27스터디+

---
+++ programmers/Lv1/23_이상한문자만들기.js
@@ -0,0 +1,33 @@
+/* 
+문자열 s는 한 개 이상의 단어로 구성되어 있습니다. 
+각 단어는 하나 이상의 공백문자로 구분되어 있습니다.
+각 단어의 짝수번째 알파벳은 대문자로, 홀수번째 알파벳은 소문자로 바꾼 문자열을 리턴하는 함수, solution을 완성하세요.
+
+제한 사항
+문자열 전체의 짝/홀수 인덱스가 아니라, 단어(공백을 기준)별로 짝/홀수 인덱스를 판단해야합니다.
+첫 번째 글자는 0번째 인덱스로 보아 짝수번째 알파벳으로 처리해야 합니다.
+
+입출력 예
+      s	                        return
+"try hello world"	    "TrY HeLlO WoRlD"
+
+*/
+
+function solution(s) {
+    let result = "";
+    let strArr = s.split(" ");
+
+    for (let i = 0; i < strArr.length; i++) {
+        for (let j = 0; j < strArr[i].length; j++) {
+            if (j % 2 == 0) {
+                result += strArr[i][j].toUpperCase();
+            } else {
+                result += strArr[i][j].toLowerCase();
+            }
+        }
+        if (!(i == strArr.length - 1)) {
+            result += " ";
+        }
+    }
+    return result;
+}
